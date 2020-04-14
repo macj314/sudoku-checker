a57209e98f2e768d0aa6736a0734662a453ecdda
unknown <macjay314@gmail.com>
2020-04-14 14:44:17 -0700

    Add test to thing.test.js.

--- __tests__/thing.test.js
+++ __tests__/thing.test.js
@@ -10,4 +10,8 @@ describe('Thing', () => {
   test('should correctly go through a board row and test if it has any duplicate numbers', () => {
     expect(board.checkRow()).toEqual("row has no duplicates");
   });
+
+  test('should correctly go through a board column and test if it has any duplicate numbers', () => {
+    expect(board.checkColumn()).toEqual("column has no duplicates");
+  });
 });
