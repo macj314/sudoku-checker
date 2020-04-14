0f416f870dbec64b2825b0292f2d7f8b0e1f4d00
unknown <macjay314@gmail.com>
2020-04-14 16:31:54 -0700

    Move array of rows into the 'checkBoardRows' funtion in board.js. Change for loop variables to properly go through each array.

--- __tests__/thing.test.js
+++ __tests__/thing.test.js
@@ -16,17 +16,7 @@ describe('Thing', () => {
   });
 
   test('should correctly go through an array of rows and test if it has any duplicate numbers', () => {
-    var filledBoard = [
-      [5, 3, 4, 6, 7, 8, 9, 1, 2]
-      [6, 7, 2, 1, 9, 5, 3, 4, 8]
-      [1, 9, 8, 3, 4, 2, 5, 6, 7]
-      [8, 5, 9, 7, 6, 1, 4, 2, 3]
-      [4, 2, 6, 8, 5, 3, 7, 9, 1]
-      [7, 1, 3, 9, 2, 4, 8, 5, 6]
-      [9, 6, 1, 5, 3, 7, 2, 8, 4]
-      [2, 8, 7, 4, 1, 9, 6, 3, 5]
-      [3, 4, 5, 2, 8, 6, 1, 7, 9]
-    ];
-    expect(board.checkBoardRows(filledBoard)).toEqual("row has no duplicates");
+
+    expect(board.checkBoardRows()).toEqual("rows have no duplicates");
   });
 });
--- src/board.js
+++ src/board.js
@@ -30,17 +30,28 @@ Board.prototype.checkColumn = function(){
 }
 
 //attempt to go through a multidimentional array
-Board.prototype.checkBoardRows = function(arrayRows){
+Board.prototype.checkBoardRows = function(){
+  var arrayRows = [
+    [5, 3, 4, 6, 7, 8, 9, 1, 2],
+    [6, 7, 2, 1, 9, 5, 3, 4, 8],
+    [1, 9, 8, 3, 4, 2, 5, 6, 7],
+    [8, 5, 9, 7, 6, 1, 4, 2, 3],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 6, 1, 5, 3, 7, 2, 8, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 4, 5, 2, 8, 6, 1, 7, 9]
+  ];
   var compareArray = [];
   for (var i = 0; i <= arrayRows.length; i++){
-    var innerArrayLength = arrayRows.length;
-    for (var j = 0; j <= innerArrayLength.length; j++){
+    var innerArrayLength = arrayRows[i].length;
+    for (var j = 0; j <= innerArrayLength; j++){
       if (compareArray[arrayRows[i][j]] === undefined){
         compareArray[arrayRows[i][j]] = 1;
       } else {
         return "this row has duplicates:" + arrayRows[i][j];
       }
     }
-    return "rows have no duplicates";
   }
+  return "rows have no duplicates";
 }
